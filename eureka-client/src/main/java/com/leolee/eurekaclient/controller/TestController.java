e2200066732691f8dcf787c6fd78f5e5ee1629ad
LeoLee <512240816@qq.com>
2020-08-17 11:01:11 +0800

    所有项目增加logback配置

--- eureka-client/src/main/java/com/leolee/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/leolee/eurekaclient/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.leolee.eurekaclient.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Value("${hello}")
     private String hello;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getConfigHello() {
+        logger.info("client log");
         return hello;
     }
 
